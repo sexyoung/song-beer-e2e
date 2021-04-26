5f66dbc6da7040b0979c28883c14a1647c53aac5
寫詩羊 <Habuche@gmail.com>
2021-04-27 01:38:47 +0800

    feat(dev): run at dev

--- cypress/integration/tsb.spec.js
+++ cypress/integration/tsb.spec.js
@@ -8,7 +8,16 @@ describe('專案', () => {
 describe('首頁', () => {
   it('有出現 `首頁` 字樣', () => {
     cy.contains('首頁').should('be.visible');
-    cy.screenshot('HomePage');
+    // cy.screenshot('HomePage');
+  });
+
+  it('有出現 `你18歲了嗎` 彈跳視窗', () => {
+    cy.contains('你18歲了嗎').should('be.visible');
+  });
+
+  it('關閉 `你18歲了嗎` 彈跳視窗', () => {
+    cy.get('[data-testid="Modal"]').parent().click();
+    cy.get('[data-testid="Modal"]').should('not.exist');
   });
 });
 
@@ -16,6 +25,15 @@ describe('掃描頁', () => {
   it('有出現ScanPage字樣', () => {
     cy.visit('/scan');
     cy.contains('ScanPage').should('be.visible');
-    cy.screenshot('ScanPage');
+    // cy.screenshot('ScanPage');
   });
+  
+  // it('出現 `Pls Loigin with!`', () => {
+  //   cy.contains('Pls Loigin with!').should('be.visible');
+  // });
+  
+  // it('出現 臉書 與 Google 登入', () => {
+  //   cy.contains('Facebook SignIn').should('be.visible');
+  //   cy.contains('Google SignIn').should('be.visible');
+  // });
 });
