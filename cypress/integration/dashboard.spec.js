37864ff4dd0421d354a2687d96894e2a8bbc628d
sasie59 <sasie59@gmail.com>
2021-06-03 00:28:47 +0800

    各自切換到sacned & winner 都成功

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -9,7 +9,7 @@ describe("Dashboard", () => {
 
     it("login success", () => {
       cy.visit(
-        "http://localhost:3001/login?token=9576c6818ac662b1449e48ec1fe0226f5a8d271b"
+        "http://localhost:3001/login?token=68003262c858d4c4cb5483b5faee725d044dc1dc"
       );
       cy.contains("掃描狀況⼤表").should("be.visible");
       cy.contains("已掃描的明細列表").should("be.visible");
@@ -18,12 +18,27 @@ describe("Dashboard", () => {
   });
   describe('已掃描頁', () => {
     it("switch to scan", () => {
-      cy.visit("http://localhost:3001/scaned");
+      cy.visit(
+        "http://localhost:3001/login?token=68003262c858d4c4cb5483b5faee725d044dc1dc"
+      );
+      cy.contains("已掃描的明細列表").click();
+      cy.contains("讀取中").should("be.visible");
+      cy.contains("全部").should("be.visible");
+      cy.contains("查詢").should("be.visible");
     });
   });
   describe('中獎頁', () => {
     it("switch to winner", () => {
-      cy.visit("http://localhost:3001/winner")
-    })
-  })
+      cy.visit(
+        "http://localhost:3001/login?token=68003262c858d4c4cb5483b5faee725d044dc1dc"
+      );
+      cy.contains("中獎者清單頁").click();
+      cy.contains("讀取中").should("be.visible");
+      // cy.contains("水杯小").should("be.visible");
+      // cy.contains("水杯大").should("be.visible");
+      // cy.contains("野餐墊").should("be.visible");
+      // cy.contains("冰 桶").should("be.visible");
+      // cy.contains("行李箱").should("be.visible");
+    });
+  });
 });
