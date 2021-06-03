745a7b925f46963bfd4b991e8cb503a06a641e20
sasie59 <sasie59@gmail.com>
2021-06-03 23:52:09 +0800

    傳至正確網站 非本機端

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -1,25 +1,26 @@
+const token ='68003262c858d4c4cb5483b5faee725d044dc1dc';
 describe("Dashboard", () => {
   describe("Login test", () => {
     it("login fail", () => {
-      cy.visit("http://localhost:3001/login?token=error");
+      cy.visit("https://tsb-dashboard.sexyoung.tw/login?token=error");
       cy.contains("掃描狀況⼤表").should("not.be.exist");
       cy.contains("已掃描的明細列表").should("not.be.exist");
       cy.contains("中獎者清單頁").should("not.be.exist");
     });
-
     it("login success", () => {
       cy.visit(
-        "http://localhost:3001/login?token=68003262c858d4c4cb5483b5faee725d044dc1dc"
+        `https://tsb-dashboard.sexyoung.tw/login?token=${token}`
       );
       cy.contains("掃描狀況⼤表").should("be.visible");
       cy.contains("已掃描的明細列表").should("be.visible");
       cy.contains("中獎者清單頁").should("be.visible");
     });
+
   });
   describe('已掃描頁', () => {
     it("switch to scan", () => {
       cy.visit(
-        "http://localhost:3001/login?token=68003262c858d4c4cb5483b5faee725d044dc1dc"
+        `https://tsb-dashboard.sexyoung.tw/login?token=${token}`
       );
       cy.contains("已掃描的明細列表").click();
       cy.contains("讀取中").should("be.visible");
@@ -30,15 +31,10 @@ describe("Dashboard", () => {
   describe('中獎頁', () => {
     it("switch to winner", () => {
       cy.visit(
-        "http://localhost:3001/login?token=68003262c858d4c4cb5483b5faee725d044dc1dc"
+        `https://tsb-dashboard.sexyoung.tw/login?token=${token}`
       );
       cy.contains("中獎者清單頁").click();
       cy.contains("讀取中").should("be.visible");
-      // cy.contains("水杯小").should("be.visible");
-      // cy.contains("水杯大").should("be.visible");
-      // cy.contains("野餐墊").should("be.visible");
-      // cy.contains("冰 桶").should("be.visible");
-      // cy.contains("行李箱").should("be.visible");
     });
   });
 });
