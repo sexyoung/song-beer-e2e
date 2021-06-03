894c15d963a879e475959e922a46ad98ea4789bb
sasie59 <sasie59@gmail.com>
2021-06-03 23:58:32 +0800

    設兩個變數

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -1,15 +1,19 @@
 const token ='68003262c858d4c4cb5483b5faee725d044dc1dc';
+
+const localhost = 'http://localhost:3001/'
+const dashboard = 'https://tsb-dashboard.sexyoung.tw/';
+
 describe("Dashboard", () => {
   describe("Login test", () => {
     it("login fail", () => {
-      cy.visit("https://tsb-dashboard.sexyoung.tw/login?token=error");
+      cy.visit("http://localhost:3001/login?token=error");
       cy.contains("掃描狀況⼤表").should("not.be.exist");
       cy.contains("已掃描的明細列表").should("not.be.exist");
       cy.contains("中獎者清單頁").should("not.be.exist");
     });
     it("login success", () => {
       cy.visit(
-        `https://tsb-dashboard.sexyoung.tw/login?token=${token}`
+        `http://localhost:3001/login?token=${token}`
       );
       cy.contains("掃描狀況⼤表").should("be.visible");
       cy.contains("已掃描的明細列表").should("be.visible");
@@ -20,7 +24,7 @@ describe("Dashboard", () => {
   describe('已掃描頁', () => {
     it("switch to scan", () => {
       cy.visit(
-        `https://tsb-dashboard.sexyoung.tw/login?token=${token}`
+        `http://localhost:3001/login?token=${token}`
       );
       cy.contains("已掃描的明細列表").click();
       cy.contains("讀取中").should("be.visible");
@@ -31,7 +35,7 @@ describe("Dashboard", () => {
   describe('中獎頁', () => {
     it("switch to winner", () => {
       cy.visit(
-        `https://tsb-dashboard.sexyoung.tw/login?token=${token}`
+        `http://localhost:3001/login?token=${token}`
       );
       cy.contains("中獎者清單頁").click();
       cy.contains("讀取中").should("be.visible");
