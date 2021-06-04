0546039e94331ccdcd9725f2ca4fdf8b2184cc7b
sasie59 <sasie59@gmail.com>
2021-06-04 22:35:31 +0800

    已掃描明細列表 腳本測式 is ok

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -1,7 +1,7 @@
-const token ='68003262c858d4c4cb5483b5faee725d044dc1dc';
+const TOKEN ='277e02155124c2cd1abc6d05e6f3065ba7fb0068';
 
-const localhost = 'http://localhost:3001/'
-const dashboard = 'https://tsb-dashboard.sexyoung.tw/';
+const LOCAL = 'http://localhost:3001/'
+const DEV = 'https://tsb-dashboard.sexyoung.tw/';
 
 describe("Dashboard", () => {
   describe("Login test", () => {
@@ -12,8 +12,9 @@ describe("Dashboard", () => {
       cy.contains("中獎者清單頁").should("not.be.exist");
     });
     it("login success", () => {
+
       cy.visit(
-        `http://localhost:3001/login?token=${token}`
+        `${LOCAL}login?token=${TOKEN}`
       );
       cy.contains("掃描狀況⼤表").should("be.visible");
       cy.contains("已掃描的明細列表").should("be.visible");
@@ -24,21 +25,58 @@ describe("Dashboard", () => {
   describe('已掃描頁', () => {
     it("switch to scan", () => {
       cy.visit(
-        `http://localhost:3001/login?token=${token}`
+        `${LOCAL}login?token=${TOKEN}`
       );
       cy.contains("已掃描的明細列表").click();
       cy.contains("讀取中").should("be.visible");
       cy.contains("全部").should("be.visible");
       cy.contains("查詢").should("be.visible");
     });
-  });
-  describe('中獎頁', () => {
-    it("switch to winner", () => {
+    it("已掃描明細列表，是否可以查詢沒中的帳號或序號？", () => {
       cy.visit(
-        `http://localhost:3001/login?token=${token}`
+        `${LOCAL}login?token=${TOKEN}`
       );
-      cy.contains("中獎者清單頁").click();
+      cy.contains("已掃描的明細列表").click();
+      cy.get('input').type('00231LC');
+      cy.get('select').select('沒中');
+      cy.get('form').submit();
       cy.contains("讀取中").should("be.visible");
-    });
+      cy.get('input').clear();
+      cy.get('input').type('越');
+      cy.get('form').submit();
+    })
+    it("已掃描明細列表，是否可以查詢中獎的帳號或序號？", () => {
+      cy.visit(
+        `${LOCAL}login?token=${TOKEN}`
+      );
+      cy.contains("已掃描的明細列表").click();
+      cy.get('input').type('meng');
+      cy.get('select').select('中獎');
+      cy.get('form').submit();
+      cy.contains("讀取中").should("be.visible");
+      cy.get('input').clear();
+      cy.get('input').type('kellyu00621');
+      cy.get('form').submit();
+      cy.contains("讀取中").should("be.visible");
+    })
+    it("已掃描明細列表，是否可以查詢全部的帳號或序號？", () => {
+      cy.visit(
+        `${LOCAL}login?token=${TOKEN}`
+      );
+      cy.contains("已掃描的明細列表").click();
+      cy.get('select').select('中獎');
+      cy.get('select').select('全部');
+      cy.get('select').select('沒中');
+      cy.get('select').select('全部');
+    })
   });
+  // describe('中獎頁', () => {
+  //   it("switch to winner", () => {
+  //     cy.visit(
+  //       `${LOCAL}login?token=${TOKEN}`
+  //     );
+  //     cy.contains("中獎者清單頁").click();
+  //     cy.contains("讀取中").should("be.visible");
+  //   });
+  // });
 });
